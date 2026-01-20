0c4a712ab37f67d0befe44c84cbff2f1c6f48755
Dave MacFarlane <driusan@driusan.net>
2026-01-20 06:00:43 +0000

    Missed a file in last commit

--- ltsarch.c
+++ ltsarch.c
@@ -7,10 +7,21 @@
 /* Stubs for compatibility with pinephone kernel. These
   are referenced by axp803 but shouldn't be. */
 int brightness;
-void backlight(int)
+void
+backlight(int)
 {
 }
 
-void subarchinit(void)
+void
+subarchinit(void)
 {
+	/* assume 1080p for now */
+	hdmiinit(1920, 1080);
+	deinit(1920, 1080);
+}
+
+int
+displayishdmi(void)
+{
+	return 1;
 }
